212db50bed32068579a865c6498051193306a336
GitHubEngine <nikita.nstu@gmail.com>
2019-10-08 20:16:31 +0700

    Asm3 (completed)

--- Asm3/Asm3/1.asm
+++ Asm3/Asm3/1.asm
@@ -3,7 +3,6 @@
 .DATA
 	A DD 0.0
 	B DD 1.5707963
-	
 
 .CODE
 	func PROC
@@ -17,26 +16,27 @@
 		MOV [EBP - 8], EAX
 
 		FINIT
-		;							ST(0)			ST(1)
-		FLD DWORD PTR [EBP + 8]  ;	x
-		FSIN					  ;	sin(x)
-		FST ST(1)				  ;	sin(x)			sin(x)
-		FMULP					  ;	sin^2(x)
-		FIMUL DWORD PTR [EBP - 4] ;	2*sin^2(x)
-		FIDIV DWORD PTR [EBP - 8] ;	2*sin^2(x)/3
-		FSTP DWORD PTR [EBP - 12] ;	Save value in [EBP - 16]
+		;								ST(0)			ST(1)
+		FLD DWORD PTR [EBP + 8]		;	x
+		FCOS						;	cos(x)
+		FST ST(1)					;	cos(x)			cos(x)
+		FMULP						;	cos^2(x)
+		FIMUL DWORD PTR [EBP - 8]	;	3*cos^2(x)
+		FIDIV DWORD PTR [EBP - 4]	;	3*cos^2(x)/2
+		FIDIV DWORD PTR [EBP - 4]	;	3*cos^2(x)/4
+		FSTP DWORD PTR [EBP - 12]	;	Save value in [EBP - 12]
 
-		;							ST(0)			ST(1)
-		FLD DWORD PTR [EBP + 8]	  ;	x
-		FCOS					  ; cos(x)
-		FST ST(1)				  ;	cos(x)			cos(x)
-		FMULP					  ; cos^2(x)
-		FIMUL DWORD PTR [EBP - 8] ; 3*cos^2(x)
-		FIDIV DWORD PTR [EBP - 4] ;	3*cos^2(x)/2
-		FIDIV DWORD PTR [EBP - 4] ; 3*cos^2(x)/4
+		;								ST(0)			ST(1)
+		FLD DWORD PTR [EBP + 8]		;	x
+		FSIN						;	sin(x)
+		FST ST(1)					;	sin(x)			sin(x)
+		FMULP						;	sin^2(x)
+		FIMUL DWORD PTR [EBP - 4]	;	2*sin^2(x)
+		FIDIV DWORD PTR [EBP - 8]	;	2*sin^2(x)/3
+	
+		FLD DWORD PTR [EBP - 12]	;	ST(0) = 2*sin^2(x)/3	ST(1) = 3*cos^2(x)/4
+		FSUBP						;	ST(0) = 2*sin^2(x)/3 - 3*cos^2(x)/4
 		
-		FLD DWORD PTR [EBP - 12]; ST(0) = 2*sin^2(x)/3	ST(1) = 3*cos^2(x)/4
-		FSUB ST, ST(1)			; ST(0) = 2*sin^2(x)/3 - 3*cos^2(x)/4
 		MOV ESP, EBP
 		POP EBP
 		RET
@@ -47,12 +47,23 @@
 	dichotomy PROC
 		PUSH EBP
 		MOV EBP, ESP
-		SUB ESP, 4
+		SUB ESP, 16
+		; [EBP - 4] - c = (a + b) / 2
+		; [EBP - 8] - f(c)
+		; [EBP - 12] - f(a)
+		; [EBP - 16] - f(b)
 
+		; number of iterations = 0
+		XOR EBX, EBX
+
+		FINIT
+		; number of iterations += 1
+BEGIN:	INC EBX
+
+		; constant = 2
 		MOV EAX, 2
 		MOV [EBP - 4], EAX
 
-		FINIT
 		; c = (a + b) / 2
 		FLD A
 		FLD B
@@ -60,8 +71,89 @@
 		FIDIV DWORD PTR [EBP - 4]
 		FSTP DWORD PTR [EBP - 4]
 
+		; Calculate f(c)
 		PUSH [EBP - 4]
 		CALL func
+		FSTP DWORD PTR [EBP - 8]
+
+		; Calculate f(a)
+		PUSH A
+		CALL func
+		FSTP DWORD PTR [EBP - 12]
+
+		; Calculate f(b)
+		PUSH B
+		CALL func
+		FSTP DWORD PTR [EBP - 16]
+
+		; Compare f(c) with 0
+		FLDZ
+		FLD DWORD PTR [EBP - 8]
+		FCOMPP
+		FSTSW AX 
+		SAHF
+
+		JE EXIT		; f(c) = 0
+		JA ABOV0	; f(c) > 0
+		JMP LESS0	; f(c) < 0
+
+		; Compare f(a) with 0
+LESS0:	FLDZ
+		FLD DWORD PTR [EBP - 12]
+		FCOMPP
+		FSTSW AX 
+		SAHF
+
+		JB ALESS0
+		; if (f(a) > 0) then b = c
+		MOV EAX, [EBP - 4]
+		MOV B, EAX
+		JMP CHECK
+
+		; if (f(a) < 0) then a = c
+ALESS0:	MOV EAX, [EBP - 4]
+		MOV A, EAX
+		JMP CHECK
+
+
+		; Compare f(a) with 0
+ABOV0:	FLDZ
+		FLD DWORD PTR [EBP - 12]
+		FCOMPP
+		FSTSW AX 
+		SAHF
+
+		JAE AABOV0
+		; if (f(a) < 0) then b = c
+		MOV EAX, [EBP - 4]
+		MOV B, EAX
+		JMP CHECK
+
+		; if (f(a) > 0) then a = c
+AABOV0: MOV EAX, [EBP - 4]
+		MOV A, EAX
+		JMP CHECK
+
+		; Find (b - a)
+CHECK:	FLD A
+		FLD B
+		FSUBP
+		FABS
+		; Compare (b - a) with eps
+		FLD DWORD PTR [EBP + 8]
+		FCOMPP
+		FSTSW AX 
+		SAHF
+
+		JA EXIT
+		JMP BEGIN
+
+		; return number of iterations
+EXIT:	MOV EAX, EBX
+		; return x
+		MOV EDX, [EBP - 4]
+		MOV EBX, [EBP + 12]
+		MOV [EBX], EDX
 
 		MOV ESP, EBP
 		POP EBP
--- Asm3/Asm3/main.cpp
+++ Asm3/Asm3/main.cpp
@@ -6,7 +6,7 @@ extern "C" int dichotomy(float eps, float& x);
 int main()
 {
 	float x = 0;
-	int res = dichotomy(0.001, x);
+	int res = dichotomy(10e-8, x);
 	printf_s("Result: %6f", x);
 	return 0;
 }
